db004b8618a856c718016875b261cb5ffda7bec1
Unknown <ryodany@hotmail.com>
2018-11-18 01:20:54 +0100

    No need to allocate on the heap.

--- c/test_wasm.c
+++ c/test_wasm.c
@@ -1,12 +1,8 @@
-#include <string.h>
-#include <stdlib.h>
 #include "include/webassembly/export.h"
 
 #define TEST_SIZE 256
 
 WASM_FUNCTION_EXPORT char *getTestStr()
 {
-    char *ret = (char *)calloc(TEST_SIZE, sizeof(char));
-    strcpy(ret, "Comeporongas marico de mierda");
-    return ret;
+    return "Comeporongas marico de mierda";
 }
